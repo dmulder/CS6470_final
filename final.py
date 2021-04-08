6a7b0c48d4addc29229a75c60c845b7b816428d3
David Mulder <dmulder@suse.com>
2021-04-07 18:21:31 -0600

    Encapsulate the classifier

--- final.py
+++ final.py
@@ -9,73 +9,83 @@ from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 import argparse
 
-data_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'plastics')
-batch_size = 32
-img_height = 180
-img_width = 180
-train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-  data_dir,
-  validation_split=0.2,
-  subset="training",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
-val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-  data_dir,
-  validation_split=0.2,
-  subset="validation",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
+class PlasticRecyclableClassifier(object):
+    def __init__(self, data_dir, load_saved_model=False):
+        batch_size = 32
+        self.img_height = 180
+        self.img_width = 180
+        train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+          data_dir,
+          validation_split=0.2,
+          subset="training",
+          seed=123,
+          image_size=(self.img_height, self.img_width),
+          batch_size=batch_size)
+        val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+          data_dir,
+          validation_split=0.2,
+          subset="validation",
+          seed=123,
+          image_size=(self.img_height, self.img_width),
+          batch_size=batch_size)
 
-class_names = train_ds.class_names
+        self.class_names = train_ds.class_names
 
-AUTOTUNE = tf.data.AUTOTUNE
+        train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+        val_ds = val_ds.cache().prefetch(buffer_size=tf.data.AUTOTUNE)
 
-train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+        normalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
 
-normalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
+        normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+        image_batch, labels_batch = next(iter(normalized_ds))
+        first_image = image_batch[0]
 
-normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
-image_batch, labels_batch = next(iter(normalized_ds))
-first_image = image_batch[0]
+        self.model = Sequential([
+          layers.experimental.preprocessing.Rescaling(1./255, input_shape=(self.img_height, self.img_width, 3)),
+          layers.Conv2D(16, 3, padding='same', activation='relu'),
+          layers.MaxPooling2D(),
+          layers.Conv2D(32, 3, padding='same', activation='relu'),
+          layers.MaxPooling2D(),
+          layers.Conv2D(64, 3, padding='same', activation='relu'),
+          layers.MaxPooling2D(),
+          layers.Flatten(),
+          layers.Dense(128, activation='relu'),
+          layers.Dense(len(self.class_names))
+        ])
 
-num_classes = len(class_names)
-
-model = Sequential([
-  layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
-  layers.Conv2D(16, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(32, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(64, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Flatten(),
-  layers.Dense(128, activation='relu'),
-  layers.Dense(num_classes)
-])
-
-model.compile(optimizer='adam',
+        self.model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
+
+        if os.path.exists('./final_weights.index') and load_saved_model:
+            self.model.load_weights('final_weights')
+        else:
+            epochs=10
+            history = self.model.fit(
+              train_ds,
+              validation_data=val_ds,
+              epochs=epochs
+            )
+            self.model.save_weights('final_weights')
+
+    def predict(self, filename):
+        img = keras.preprocessing.image.load_img(file_path, target_size=(self.img_height, self.img_width))
+        img_array = keras.preprocessing.image.img_to_array(img)
+        img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+        predictions = self.model.predict(img_array)
+        score = tf.nn.softmax(predictions[0])
+        return self.class_names[np.argmax(score)], 100 * np.max(score)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--load-saved-model', action='store_true', help='Load a saved model')
+    default_data_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'plastics')
+    parser.add_argument('--data-dir', help='Location of the plastics data directory', default=default_data_dir)
 
     args = parser.parse_args()
 
-    if os.path.exists('./final_weights.index') and args.load_saved_model:
-        model.load_weights('final_weights')
-    else:
-        epochs=10
-        history = model.fit(
-          train_ds,
-          validation_data=val_ds,
-          epochs=epochs
-        )
-        model.save_weights('final_weights')
+    classifier = PlasticRecyclableClassifier(args.data_dir, args.load_saved_model)
 
     while True:
         filename = input('Specify an image to predict: ')
@@ -85,14 +95,9 @@ if __name__ == "__main__":
         if not os.path.exists(file_path):
             print('File does not exist')
             continue
-        img = keras.preprocessing.image.load_img(file_path, target_size=(img_height, img_width))
-        img_array = keras.preprocessing.image.img_to_array(img)
-        img_array = tf.expand_dims(img_array, 0) # Create a batch
-
-        predictions = model.predict(img_array)
-        score = tf.nn.softmax(predictions[0])
+        class_name, score = classifier.predict(file_path)
 
         print(
             "This image most likely belongs to {} with a {:.2f} percent confidence."
-            .format(class_names[np.argmax(score)], 100 * np.max(score))
+            .format(class_name, score)
         )
